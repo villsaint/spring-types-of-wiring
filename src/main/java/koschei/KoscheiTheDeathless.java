f691ebed15501ae46f12709d402fdfa0f6395c2d
villsaint <jackspow18@gmail.com>
2021-03-23 02:23:11 +0300

    Finish

--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -10,11 +10,13 @@ public class KoscheiTheDeathless {
     private Ocean1 ocean;
 
     public String getRulesByDeth() {
+
         return "На свете есть океан , " + ocean.toString();
     }
 
     @Autowired
     public void setOcean(Ocean1 ocean) {
+
         this.ocean = ocean;
     }
 }
